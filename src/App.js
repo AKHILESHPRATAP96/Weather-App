c4ec76c144575e6f382e4273c7a3de9b883b7b24
AKHILESHPRATAP96 <114492226+AKHILESHPRATAP96@users.noreply.github.com>
2024-03-10 18:30:40 +0530

    fix ui

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 import SearchForm from "./components/SearchForm";
+import axios from "axios";
 
 function App() {
+ 
   return (
     <div className="App">
       <div className="container">
         <div className="weather-app-wrapper">
           <div className="weather-app rounded-4">
-            <SearchForm defaultCity="Bangalore" />
+            <SearchForm defaultCity={"bangalore"} />
           </div>
         </div>
       </div>
